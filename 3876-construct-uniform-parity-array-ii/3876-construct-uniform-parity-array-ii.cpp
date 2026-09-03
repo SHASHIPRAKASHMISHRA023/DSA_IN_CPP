9f949ac4fdf6d70230f5136e1e74a9cc6394eeb8
SHASHIPRAKASHMISHRA023 <shashiprakashmishra0230@gmail.com>
2026-09-04 00:59:01 +0530

    Time: 0 ms (100%), Space: 165.9 MB (62.83%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
         int mn = *min_element(nums1.begin(),nums1.end());
-        if(mn%2 == 1){
-            return true;
+        if(mn%2 == 1){//odd-then we can convert all even to odd
+            return true;//all nums2 will be odd
         }
+        //check if we can make all even in nums2
         for(int nums:nums1){
             if(nums%2 == 1){
                 return false;
